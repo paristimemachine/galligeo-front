8041e81aff4c7acd4cdf66a03c96eaa07f3a2851
Eric <19222193+ericgrosso@users.noreply.github.com>
2025-05-22 11:37:35 +0200

    Add Besancon atlas

---
+++ atlas/besancon/js/besancon_layers.js
@@ -0,0 +1,72 @@
+// Bing
+const bingAerial = new L.BingLayer('AtpNIxEZLSN40SwkHvqVlUMSwMExSusN9Ga7g3bkmFTe8ncCrS9hLlJqHq1qR-WE', {
+  maxZoom: 19
+});
+
+const layerNames = [ "1895", "1892", "1845", "1838" ]
+const layerIds = [ "btv1b530761213", "btv1b8445742g", "btv1b84432387", "btv1b531027643" ]
+const URL_TILE_SERVER = "https://tile.ptm.huma-num.fr/tiles/ark/";
+const URL_TILE_SERVER_SUB = "https://{s}.tile.ptm.huma-num.fr/tiles/ark/";
+const params = new URLSearchParams(document.location.search);
+// const ark = params.get("ark");
+
+async function getLayerFromArk(ark, name) {
+  const urlInfo = URL_TILE_SERVER + "info_tiles/12148/" + ark;
+
+  let response = await fetch(urlInfo);
+  if (response.ok) {
+    let rjson = await response.json();
+    const bounds = rjson.bounds.split(",");
+    const long = (parseFloat(bounds[0]) + parseFloat(bounds[2])) / 2;
+    const lat = (parseFloat(bounds[1]) + parseFloat(bounds[3])) / 2;
+    const zoom = Math.floor((parseInt(rjson.minzoom) + parseInt(rjson.maxzoom)) / 2);
+
+    const corner1 = L.latLng(parseFloat(bounds[1]), parseFloat(bounds[0]));
+    const corner2 = L.latLng(parseFloat(bounds[3]), parseFloat(bounds[2]));
+    const tileBounds = L.latLngBounds(corner1, corner2);
+
+    let layer = new L.tileLayer(URL_TILE_SERVER_SUB + '12148/' + ark + '/{z}/{x}/{y}.png', {
+      minNativeZoom: parseInt(rjson.minzoom),
+      maxNativeZoom: parseInt(rjson.maxzoom),
+      minZoom: 4,
+      maxZoom: 19,
+      bounds: tileBounds,
+      attribution: '&copy; <a href="https://gallica.bnf.fr/ark:/12148/' + ark + '/" target="_blank">BnF - Gallica</a> / PTM - Galligeo',
+      name: name
+    });
+
+    return layer;
+  } else {
+    throw new Error("La référence ARK (" + ark + ") ne semble pas encore être géoréférencée.");
+    return null;
+  }
+}
+
+async function getOverlays() {
+  let overlayers = [ bingAerial ];
+  for (let i = 0; i < layerNames.length; i++) {
+    let l = await getLayerFromArk(layerIds[i], layerNames[i]);
+    overlayers.push(l);
+  }
+  return overlayers;
+}
+
+
+/*  
+  function displayMessage(message) {
+    const div = document.getElementById("map");
+    const content = document.createTextNode(message);
+    div.appendChild(content);
+    // document.body.append(div);
+  }
+*/
+
+/*
+    const slider = document.getElementById("opacityRange");
+    slider.style.display = "block";
+
+    slider.oninput = function() {
+      galligeoLayer.setOpacity(this.value / 100);
+    }
+  }
+*/
---
+++ atlas/besancon/js/map.js
@@ -0,0 +1,82 @@
+let map;
+let overlays = [];
+let overlaysFlags = [];
+const lat = 47.235;
+const long = 6.027;
+const mapZoom = 16;
+const mapMaxZoom = 19
+const mapPreferCanvas = true;
+let currentLayer;
+
+const slider = document.getElementById("opacityRange");
+
+let osm = new L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    // attribution: 'Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Tiles courtesy of <a href="http://openstreetmap.org/">OpenStreetMap</a>',
+    maxNativeZoom: 18,
+    maxZoom: mapMaxZoom
+});
+
+async function init() {
+  overlays = await getOverlays();
+
+  map = new L.Map('map', {
+    center: new L.LatLng(lat, long),
+    zoom: mapZoom,
+    maxZoom: mapMaxZoom,
+    preferCanvas: mapPreferCanvas
+  });
+
+  osm.addTo(map);
+
+  for (let i = 0 ; i < overlays.length ; i++) {
+    overlays[i].setZIndex(i+1);
+    overlaysFlags.push(false);
+  }
+  initSlider();
+}
+
+(async () => {
+    await init();
+})();
+
+function addRemoveOverlayMap(overlayIndex) {
+  let index = overlaysFlags.indexOf(true);
+
+  if (index === -1) {
+    slider.value = 100;
+    currentLayer = overlays[overlayIndex];
+    map.addLayer(overlays[overlayIndex]);
+    overlaysFlags[overlayIndex] = true;
+    document.getElementById("button" + overlayIndex).style.fontWeight = 'bold';
+  } else {
+    if (index === overlayIndex) {
+      slider.value = 100;
+      currentLayer.setOpacity(1);
+      currentLayer = null;
+      map.removeLayer(overlays[overlayIndex]);
+      overlaysFlags[overlayIndex] = false;
+      document.getElementById("button" + index).style.fontWeight = 'normal';
+    } else {
+      map.removeLayer(overlays[index]);
+      overlaysFlags[index] = false;
+      document.getElementById("button" + index).style.fontWeight = 'normal';
+      slider.value = 100;
+      currentLayer.setOpacity(1);
+      currentLayer = overlays[overlayIndex];
+      map.addLayer(overlays[overlayIndex]);
+      overlaysFlags[overlayIndex] = true;
+      document.getElementById("button" + overlayIndex).style.fontWeight = 'bold';
+    }
+  }
+}
+
+function initSlider() {
+  slider.style.display = "block";
+
+  slider.oninput = function() {
+    if (currentLayer) {
+      currentLayer.setOpacity(this.value / 100);
+    }
+  }
+}
